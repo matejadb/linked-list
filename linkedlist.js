4d06c37793c9c030023c0ec56ffa7fe7a7e3aee6
matejadb <92729211+matejadb@users.noreply.github.com>
2025-03-19 10:32:27 +0100

    Make append and toString functions

---
+++ linkedlist.js
@@ -0,0 +1,34 @@
+import { Node } from './node.js';
+
+export class LinkedList {
+	constructor(head = null) {
+		this.head = head;
+	}
+
+	append(value) {
+		let newNode = new Node(value);
+		if (!this.head) {
+			this.head = newNode;
+			return;
+		}
+		let current = this.head;
+
+		while (current.nextNode) {
+			current = current.nextNode;
+		}
+		current.nextNode = newNode;
+	}
+
+	toString() {
+		let printList = ``;
+		let current = this.head;
+
+		while (current) {
+			printList += `( ${current.value} ) -> `;
+			current = current.nextNode;
+		}
+		printList += `null`;
+
+		return printList;
+	}
+}
